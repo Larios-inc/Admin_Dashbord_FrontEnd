feb7e464b8aec8354b4065bc7fcd4c3d4381a8d5
David Castellanos <davcastellanoslarios@gmail.com>
2023-07-10 17:52:00 -0600

    update store done

--- app/(dashboard)/[storeId]/(routes)/settings/components/SettingsFrom.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/SettingsFrom.tsx
@@ -5,6 +5,9 @@ import { Store } from "@prisma/client"
 import { Trash } from "lucide-react"
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { toast } from 'react-hot-toast'
+import axios from 'axios'
+import { useParams, useRouter } from 'next/navigation'
 
 import { Heading } from "@/components/ui/Heading"
 import { Button } from "@/components/ui/button"
@@ -29,6 +32,8 @@ type SettingsFormValues = z.infer<typeof formSchema>
 
 const SettingsFrom: React.FC<SettingsFormProps> = ({ initialData }) => {
 
+    const params = useParams()
+    const router = useRouter()
     const [open, setOpen]= useState(false)
     const [loading, setLoading]= useState(false)
 
@@ -38,7 +43,18 @@ const SettingsFrom: React.FC<SettingsFormProps> = ({ initialData }) => {
     })
 
     const onSubmit = async (data: SettingsFormValues) =>{
-        console.log(data);
+        try {
+            
+            setLoading(true)
+            await axios.patch(`/api/stores/${params.storeId}`, data);
+            router.refresh()
+            toast.success("Store updated.")
+            
+        } catch (error) {
+            toast.error('Something went wrong')
+        }finally{
+            setLoading(false)
+        }
         
     }
 
