29b374f9a5cfc5ce370a50911e20d8ae515ebd74
David Castellanos <davcastellanoslarios@gmail.com>
2023-07-25 11:23:59 -0600

    order into dashboard

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { Heading } from "@/components/ui/Heading"
+import { Separator } from "@/components/ui/separator"
+
+import { OrderColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table"
+
+interface OrderClientProps {
+    data:OrderColumn[]
+}
+
+const OrderClient: React.FC<OrderClientProps> = ({
+    data
+}) => {
+
+  return (
+    <>
+        <Heading 
+            title={`Orders (${data.length})`}
+            description="Manage orders for your stores"
+        />
+        <Separator />
+        <DataTable searchKey="products" columns={columns} data={data} />
+    </>
+  )
+}
+
+export default OrderClient
